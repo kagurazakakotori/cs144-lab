3f14a04dd1f2dbad03cc0541469ec098f140d840
Kagurazaka Kotori <kagurazakakotori@gmail.com>
2020-08-16 16:10:34 +0000

    remove dummy code

--- libsponge/byte_stream.cc
+++ libsponge/byte_stream.cc
@@ -4,15 +4,7 @@
 #include <iterator>
 #include <stdexcept>
 
-// Dummy implementation of a flow-controlled in-memory byte stream.
-
-// For Lab 0, please replace with a real implementation that passes the
-// automated checks run by `make check_lab0`.
-
-// You will need to add private members to the class declaration in `byte_stream.hh`
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+// Implementation of a flow-controlled in-memory byte stream.
 
 using namespace std;
 
