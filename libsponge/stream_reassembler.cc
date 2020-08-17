160d12b5e364695059b821ee5ad7ac6ae1a67465
Kagurazaka Kotori <kagurazakakotori@gmail.com>
2020-08-17 08:17:56 +0000

    lab1 finished

--- libsponge/stream_reassembler.cc
+++ libsponge/stream_reassembler.cc
@@ -1,14 +1,6 @@
 #include "stream_reassembler.hh"
 
-// Dummy implementation of a stream reassembler.
-
-// For Lab 1, please replace with a real implementation that passes the
-// automated checks run by `make check_lab1`.
-
-// You will need to add private members to the class declaration in `stream_reassembler.hh`
-
-template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+// Implementation of a stream reassembler.
 
 using namespace std;
 
@@ -18,9 +10,63 @@ StreamReassembler::StreamReassembler(const size_t capacity) : _output(capacity),
 //! possibly out-of-order, from the logical stream, and assembles any newly
 //! contiguous substrings and writes them into the output stream in order.
 void StreamReassembler::push_substring(const string &data, const size_t index, const bool eof) {
-    DUMMY_CODE(data, index, eof);
+    _has_eof |= eof;
+
+    // ignore empty or outputed segments
+    if (data.empty() || index + data.size() <= _next_index) {
+        if (empty() && _has_eof) {
+            _output.end_input();
+        }
+        return;
+    }
+
+    // trim input to ensure index >= _next_index
+    size_t trimmed_index = max(index, _next_index);
+    size_t trimmed_size = data.size() - (trimmed_index - index);
+
+    // ensure no overlapping segments
+    for (auto iter = _unassembled_segments.begin(); iter != _unassembled_segments.end(); /* NOTHING */) {
+        auto trimmed_end = trimmed_index + trimmed_size;
+        auto segment_index = iter->first;
+        auto segment_end = segment_index + iter->second.size();
+
+        if (trimmed_index >= segment_index && trimmed_end <= segment_end) {  // ignore when data is subset of a segment
+            return;
+        } else if (trimmed_index <= segment_index && trimmed_end >= segment_end) {  // segment is subset of data, remove
+            _unassembled_bytes -= iter->second.size();
+            iter = _unassembled_segments.erase(iter);
+            continue;
+        } else if (trimmed_index < segment_index && trimmed_end > segment_index) {
+            trimmed_size = segment_index - trimmed_index;
+        } else if (trimmed_index < segment_end && trimmed_end > segment_end) {
+            trimmed_index = segment_end;
+            trimmed_size = trimmed_end - segment_end;
+        }
+        iter++;
+    }
+
+    // insert the segment
+    _unassembled_bytes += trimmed_size;
+    _unassembled_segments[trimmed_index] = data.substr(trimmed_index - index, trimmed_size);
+
+    // try to output as much as it can
+    for (auto iter = _unassembled_segments.begin(); iter != _unassembled_segments.end(); /* NOTHING */) {
+        if (iter->first != _next_index) {
+            break;
+        }
+
+        size_t bytes_written = _output.write(iter->second);
+        _unassembled_bytes -= bytes_written;
+        _next_index += bytes_written;
+
+        iter = _unassembled_segments.erase(iter);
+    }
+
+    if (empty() && _has_eof) {
+        _output.end_input();
+    }
 }
 
-size_t StreamReassembler::unassembled_bytes() const { return {}; }
+size_t StreamReassembler::unassembled_bytes() const { return _unassembled_bytes; }
 
-bool StreamReassembler::empty() const { return {}; }
+bool StreamReassembler::empty() const { return _unassembled_segments.empty(); }
--- libsponge/stream_reassembler.hh
+++ libsponge/stream_reassembler.hh
@@ -4,16 +4,19 @@
 #include "byte_stream.hh"
 
 #include <cstdint>
+#include <map>
 #include <string>
 
 //! \brief A class that assembles a series of excerpts from a byte stream (possibly out of order,
 //! possibly overlapping) into an in-order byte stream.
 class StreamReassembler {
   private:
-    // Your code here -- add private members as necessary.
-
     ByteStream _output;  //!< The reassembled in-order byte stream
     size_t _capacity;    //!< The maximum number of bytes
+    std::map<size_t, std::string> _unassembled_segments{};
+    size_t _unassembled_bytes = 0;
+    size_t _next_index = 0;
+    bool _has_eof = false;
 
   public:
     //! \brief Construct a `StreamReassembler` that will store up to `capacity` bytes.
